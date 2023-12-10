71245151ead9e66d5da3fa47d3a7f86dd8a87c90
rurifaqih15 <rs12342@gmail.com>
2023-12-10 17:22:09 +0700

    add FE code login and CRUD reminders using blade

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/reminder-list', function () {
+    return view('reminder-list');
 });
